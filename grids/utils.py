Bug fix to the integration calculation. The horizon would be colored becuase the los looked at voxels in the oposite direction.

grids/utils.py
@@ -47,6 +47,10 @@ def integrateGrids(camera_center, Y, X, Z, image_res, pixel_fov):
     # Calculate open and centered grids
     #
     (Y, X, Z), (Y_open, X_open, Z_open) = processGrids((Y, X, Z))
+    
+    #
+    # Calculate the unit vector direction for each voxel
+    #
     Y = Y - camera_center[0]
     X = X - camera_center[1]
     Z = Z - camera_center[2]
@@ -54,6 +58,10 @@ def integrateGrids(camera_center, Y, X, Z, image_res, pixel_fov):
     Y = Y/R
     X = X/R
     Z = Z/R
+    
+    #
+    # Calculate the radius of each voxel
+    #
     R_voxel = np.sqrt(
         (Y_open[1:]-Y_open[:-1]).reshape((-1, 1, 1))**2 +
         (X_open[1:]-X_open[:-1]).reshape((1, -1, 1))**2 +
@@ -61,7 +69,7 @@ def integrateGrids(camera_center, Y, X, Z, image_res, pixel_fov):
     )
     
     #
-    # Calculate the relation between sensor pixel and line of sight (LOS) vector
+    # Calculate the relation between sensor pixel and line of sight (LOS) unit vector
     #
     Y_sensor, X_sensor = np.mgrid[-1:1:complex(0, image_res), -1:1:complex(0, image_res)]
     PHI_los = np.arctan2(Y_sensor, X_sensor) + np.pi
@@ -95,13 +103,13 @@ def integrateGrids(camera_center, Y, X, Z, image_res, pixel_fov):
         #
         # Calculate the distance between all voxels to the ray.
         #
-        D = np.abs(R * sin_THETA)
-        R_cone = np.abs(R * cos_THETA) * pixel_fov
+        D_voxel_ray = np.abs(R * sin_THETA)
+        R_cone = R * cos_THETA * pixel_fov
         
         #
-        # Check intersecting Indices
+        # Find voxels intersecting the cone
         #
-        II = (R_cone + R_voxel) > D
+        II = ((R_cone + R_voxel) > D_voxel_ray) * (cos_THETA > 0)
         nnz_inds = np.flatnonzero(II)
         indices.append(nnz_inds)
         data.append(np.ones(nnz_inds.size) / nnz_inds.size)

tests/test.py
@@ -13,18 +13,18 @@ import matplotlib.pyplot as plt
 
 atmosphere_params = amitibo.attrClass(
     cartesian_grids=(
-        slice(0, 300., 3), # Y
-        slice(0, 300., 3), # X
-        slice(0, 300., 3)  # H
+        slice(0, 300., 10), # Y
+        slice(0, 300., 10), # X
+        slice(0, 300., 10)  # H
         ),
 )
 
 camera_center = (150.0, 150.0, .2)
-phi = np.pi/4
+phi = np.pi/8
 theta = np.pi/4
 Y, X, H = np.mgrid[atmosphere_params.cartesian_grids]
 sensor_res = 128
-pixel_fov = 0.1
+pixel_fov = 0.03
 
 
 def point():
@@ -37,8 +37,9 @@ def point():
     print time.time() - t0
 
     #x = np.ones(Y.size).reshape((-1, 1))
-    x = (H>100).reshape((-1, 1)).astype(np.float)
-    y = H_dist * x
+    x = np.exp(-H/10)
+    x[H<100] = 0
+    y = H_dist * x.reshape((-1, 1))
 
     amitibo.viz3D(Y, X, H, x.reshape(Y.shape), interpolation='nearest_neighbour')
     amitibo.viz3D(Y, X, H, y.reshape(Y.shape), interpolation='nearest_neighbour')
@@ -54,7 +55,7 @@ def direction():
     
     print time.time() - t0
 
-    x = (Y<10).reshape((-1, 1)).astype(np.float)
+    x = (Y<100).reshape((-1, 1)).astype(np.float)
     #x = np.ones(Y.size).reshape((-1, 1))
     y = H_dist * x
     
@@ -70,8 +71,11 @@ def integrate():
     
     print time.time() - t0
 
-    x = (Y<50).reshape((-1, 1))
-    y = H_int * x
+    x = np.ones(Y.shape)
+    #x = np.exp(-H/10)
+    x[X<150] = 0
+    y = H_int * x.reshape((-1, 1))
+
 
     plt.imshow(y.reshape((sensor_res, sensor_res)))
     plt.colorbar()
@@ -79,8 +83,8 @@ def integrate():
         
 
 if __name__ == '__main__':
-    point()
+    #point()
     #direction()
-    #integrate()
+    integrate()
 
       